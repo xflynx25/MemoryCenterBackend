9d4c5c418c8a57731e89610ee2f61dd00e874244
xflynx25 <59663716+xflynx25@users.noreply.github.com>
2023-11-24 21:08:51 -0500

    postgress preps

--- MemoryCenterBackend/settings/__init__.py
+++ MemoryCenterBackend/settings/__init__.py
@@ -5,13 +5,26 @@ from .base import *
 
 # Override with environment-specific settings
 import os
+import dj_database_url
 import importlib
 environment = os.getenv('DJANGO_ENV')
 print('env is ', environment)
 
+# Default to SQLite for local development
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Override with Postgres if DATABASE_URL is present (e.g., on Heroku)
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(os.environ.get('DATABASE_URL'))
+
+# Load environment-specific settings
 if environment:
     module = importlib.import_module(f'.{environment}', APPLICATION_NAME + '.settings')
     for setting in dir(module):
         if setting.isupper():
             locals()[setting] = getattr(module, setting)
-
